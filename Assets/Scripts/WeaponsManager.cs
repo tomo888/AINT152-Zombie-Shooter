9c7b0d5956c18634795cedb400656c3afc5099c4
tomo888 <33130687+tomo888@users.noreply.github.com>
2018-05-16 21:28:57 +0100

    Added free to use tile pack file

--- Assets/Scripts/WeaponsManager.cs
+++ Assets/Scripts/WeaponsManager.cs
@@ -10,52 +10,47 @@ using UnityEngine.Events;
 public class WeaponsManager : MonoBehaviour 
 {
 	// current stores the currently active weapon (the child gameobject switched on)
-    public int weaponChoice = 0;
+	Transform current = null;
 
 	void Start() 
 	{
-        ChangeWeapon(0);
+		// select the first weapon by default
+		ChangeWeapon(0);
 	}
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            weaponChoice = 0;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >=2)
-        {
-            weaponChoice = 1;
-        }
-    }
-
-    // this sets the weapon using the provided index
-    // it will switch off all the other child gameobjects of this one
-    void ChangeWeapon (int index) 
+	// this sets the weapon using the provided index
+	// it will switch off all the other child gameobjects of this one
+	public void ChangeWeapon (int index) 
 	{
-        int i = 0;
-		foreach (Transform weapon in transform)
-        {
-            if (i == weaponChoice)
-            {
-                weapon.gameObject.SetActive(true);
-            }
-            else
-            {
-                weapon.gameObject.SetActive(false);
-            }
-            i++;
-        }
+		// loop through all the child gameobjects
+		// get a cound of how many children using transform.childCount
+		for(int i = 0; i < transform.childCount; i++)
+		{
+			// if this child gameobject is the same one as the index, switch it on
+			if(i == index)
+			{
+				// set the current weapon as this child
+				current = transform.GetChild(index);
+
+				// switch this child gameobject on, ready for shooting
+				// use gameobject.setactive to switch it on
+				current.gameObject.SetActive(true);
+			}
+			else // if this child gameobject is not the same as the index, switch it off
+			{
+				// use gameobject.setactive to switch it off
+				transform.GetChild(i).gameObject.SetActive(false);
+			}
+		}	
 	}
-	
-	// this sends a message to the selected weapon to fire
-	// make sure weapons have a "Fire" method to use
-	public void Fire() 
+
+    // this sends a message to the selected weapon to fire
+    // make sure weapons have a "Fire" method to use
+    public void Fire () 
 	{
 		// send a message to the current weapon to fire
 		// note the last parameter "SendMessageOptions.DontRequireReceiver"
 		// this will stop any errors if the thing we are messaging doesn't have a "TakeDamage" method with an integer
-		weaponChoice.SendMessage("Fire", SendMessageOptions.DontRequireReceiver);
+		current.SendMessage("Fire", SendMessageOptions.DontRequireReceiver);
 	}
 }
